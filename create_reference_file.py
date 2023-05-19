96fa16cc4f5d98fc6dc45c42ae19e620178643fb
puneet-jain159 <puneet.jain@databricks.com>
2023-05-19 21:22:27 +0100

    adding workflows to it

--- create_reference_file.py
+++ create_reference_file.py
@@ -30,48 +30,48 @@ dbutils.notebook.run("./initialize_config", 300 ,{"catalog_name" : catalog_name,
 
 # COMMAND ----------
 
-# raw_source_paths = [
-#     "T32UME_20230419T102601_AOT_60m.jp2",
-#     "T32UME_20230419T102601_B01_60m.jp2",
-#     "T32UME_20230419T102601_B02_60m.jp2",
-#     "T32UME_20230419T102601_B03_60m.jp2",
-#     "T32UME_20230419T102601_B04_60m.jp2",
-#     "T32UME_20230419T102601_B05_60m.jp2",
-#     "T32UME_20230419T102601_B06_60m.jp2",
-#     "T32UME_20230419T102601_B07_60m.jp2",
-#     "T32UME_20230419T102601_B09_60m.jp2",
-#     "T32UME_20230419T102601_B11_60m.jp2",
-#     "T32UME_20230419T102601_B12_60m.jp2",
-#     "T32UME_20230419T102601_B8A_60m.jp2",
-#     "T32UME_20230419T102601_SCL_60m.jp2",
-#     "T32UME_20230419T102601_TCI_60m.jp2",
-#     "T32UME_20230419T102601_WVP_60m.jp2",
-# ]
+raw_source_paths = [
+    "T32UME_20230419T102601_AOT_60m.jp2",
+    "T32UME_20230419T102601_B01_60m.jp2",
+    "T32UME_20230419T102601_B02_60m.jp2",
+    "T32UME_20230419T102601_B03_60m.jp2",
+    "T32UME_20230419T102601_B04_60m.jp2",
+    "T32UME_20230419T102601_B05_60m.jp2",
+    "T32UME_20230419T102601_B06_60m.jp2",
+    "T32UME_20230419T102601_B07_60m.jp2",
+    "T32UME_20230419T102601_B09_60m.jp2",
+    "T32UME_20230419T102601_B11_60m.jp2",
+    "T32UME_20230419T102601_B12_60m.jp2",
+    "T32UME_20230419T102601_B8A_60m.jp2",
+    "T32UME_20230419T102601_SCL_60m.jp2",
+    "T32UME_20230419T102601_TCI_60m.jp2",
+    "T32UME_20230419T102601_WVP_60m.jp2",
+]
 
-# raw_df = spark.createDataFrame(
-#     [("/dbfs/FileStore/geospatial/jp2", p) for p in raw_source_paths], "prefix STRING, file_name STRING"
-# )
-# raw_df.display()
-
-# COMMAND ----------
-
-# raw_df.write.mode("overwrite").option("overwriteSchema", "true").saveAsTable(f"{schema_name}.{raw_filename}")
+raw_df = spark.createDataFrame(
+    [("/dbfs/FileStore/geospatial/jp2", p) for p in raw_source_paths], "prefix STRING, file_name STRING"
+)
+raw_df.display()
 
 # COMMAND ----------
 
-from pyspark.sql.functions import *
+raw_df.write.mode("overwrite").option("overwriteSchema", "true").saveAsTable(f"{schema_name}.{raw_filename}")
 
-spark.readStream.format("cloudFiles") \
-  .option("cloudFiles.format", "binaryFile") \
-  .load("dbfs:/FileStore/geospatial/jp2") \
-  .withColumn("p", split(col('path'), '/'))\
-  .withColumn("file_name",element_at(col('p'), -1))\
-  .withColumn("prefix", lit(raw_image_location.replace('dbfs:','/dbfs')))\
-  .select('prefix','file_name')\
-  .writeStream \
-  .option("checkpointLocation", chk_point) \
-  .trigger(once=True) \
-  .toTable(f"{schema_name}.{raw_filename}")
+# COMMAND ----------
+
+# from pyspark.sql.functions import *
+
+# spark.readStream.format("cloudFiles") \
+#   .option("cloudFiles.format", "binaryFile") \
+#   .load("dbfs:/FileStore/geospatial/jp2") \
+#   .withColumn("p", split(col('path'), '/'))\
+#   .withColumn("file_name",element_at(col('p'), -1))\
+#   .withColumn("prefix", lit(raw_image_location.replace('dbfs:','/dbfs')))\
+#   .select('prefix','file_name')\
+#   .writeStream \
+#   .option("checkpointLocation", chk_point) \
+#   .trigger(once=True) \
+#   .toTable(f"{schema_name}.{raw_filename}")
 
 # COMMAND ----------
 

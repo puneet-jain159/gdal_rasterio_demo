3f533fdb59473bfbb7e06a98c10843964f22e4a6
puneet-jain159 <puneet.jain@databricks.com>
2023-05-19 21:57:10 +0100

    correct bronze transform

--- bronze_transformation.py
+++ bronze_transformation.py
@@ -46,7 +46,7 @@ dbutils.notebook.run("./initialize_config", 300 ,{"catalog_name" : catalog_name,
 
 # Get Raw file information
 # df_raw = spark.readStream.table(f"{schema_name}.{raw_filename}")
-# df_raw = spark.read.table(f"{schema_name}.{raw_filename}")
+df_raw = spark.read.table(f"{schema_name}.{raw_filename}")
 
 # COMMAND ----------
 
